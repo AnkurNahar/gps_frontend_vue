0bac77ef5fafbe6904023d9c8d0ab3aeb15a61ff
AnkurNahar <ankurnahar.an@gmail.com>
2024-11-29 12:40:30 -0800

    map component

---
+++ src/components/MapLoader.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { GoogleMap, Marker } from 'vue3-google-map'
+
+    const center = { lat: 40.689247, lng: -74.044502 }
+    const markerOptions = { 
+        position: center, 
+        icon: {
+            url: 'https://icons.iconarchive.com/icons/icons-land/transporter/256/Car-Top-Red-icon.png',
+            scaledSize: { width: 50, height: 50 } 
+        } 
+    }
+</script>
+
+<template>
+  <GoogleMap
+    api-key=""
+    style="width: 100%; height: 800px"
+    :center="center"
+    :zoom="18"
+  >
+    <Marker :options="markerOptions" />
+  </GoogleMap>
+</template>
--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,9 @@
 <script setup>
   import Navbar from "@/components/Navbar.vue"
+  import MapLoader from "@/components/MapLoader.vue";
 </script>
 
 <template>
   <Navbar />
+  <MapLoader />
 </template>
